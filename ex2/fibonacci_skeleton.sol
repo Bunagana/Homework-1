e1fe34c14ded2f79b30d149c99d9381fdbcf48f3
Bunagana <melbinger2008@gmail.com>
2017-09-15 09:55:53 -0700

    ex1 & ex2

--- ex2/fibonacci_skeleton.sol
+++ ex2/fibonacci_skeleton.sol
@@ -4,7 +4,16 @@ pragma solidity ^0.4.16;
 contract Fibonacci {
 	function calculate(uint position) returns (uint result) {
 		/* Add one variable to hold our greeting */
+		uint a = 1;
+		uint b = 1;
+		while(position > 1) {
+			a = b;
+			b = a + b;
+			position = position - 1;
+		}
+		result = a;
 	}
 
 	/* Add a fallback function to prevent contract payability and non-existent function calls */
+	function() { throw; /*payable*/}
 }
